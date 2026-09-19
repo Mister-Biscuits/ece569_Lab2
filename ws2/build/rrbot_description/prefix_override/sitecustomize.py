1974dd40f24dd0fb91e657922e868e1e49aaf521
Skyler Choplin <schoplin01@gmail.com>
2026-09-19 13:59:27 -0400

    Finished Step 2

---
+++ ws2/build/rrbot_description/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/skyler/Documents/ece569-fall2026/ece569_Lab2/ws2/install/rrbot_description'
